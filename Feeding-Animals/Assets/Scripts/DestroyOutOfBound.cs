7970590bb1b6855ef7cdd2c1767094c18f118cd8
Bhawesh02 <bhaweshagarwal02@gmail.com>
2022-08-31 15:27:25 +0530

    Update DestroyOutOfBound.cs

--- Feeding-Animals/Assets/Scripts/DestroyOutOfBound.cs
+++ Feeding-Animals/Assets/Scripts/DestroyOutOfBound.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DestroyOutOfBound : MonoBehaviour
 {
     private float topBound = 25.0f;
-    private float bellowBound = -1.0f;
+    private float bellowBound = -5.0f;
     // Start is called before the first frame update
     void Start()
     {
